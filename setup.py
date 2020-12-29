0f4124c3ef2b97b81ab3874218e965a2384a626b
Sandro <sandro.bjelogrlic@gmail.com>
2020-12-29 11:42:59 +0100

    first commit, setup.py file

---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setuptools.setup(
+    name="targen",
+    version="0.1",
+    description="Tools for machine learning model validation",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="Sandro Bjelogrlic",
+    author_email="sandro.bjelogrlic@gmail.com",
+    license="Open Source",
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(
+        exclude=["notebooks"]
+    ),
+    install_requires=[
+        "scikit-learn>=0.22.2",
+        "pandas>=0.25",
+        "matplotlib>=3.1.1",
+        "scipy>=1.4.0",
+        "joblib>=0.13.2",
+
+    ],
+    url="https://gitlab.com/sandro.bjelogrlic/targen",
+    zip_safe=False,
+)
